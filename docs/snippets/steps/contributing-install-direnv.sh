9e1a5f8291d9597aa4009e9decc3ad9e6678f92e
Martin Jesper Low Madsen <martin@martinjlowm.dk>
2025-01-19 01:47:17 +0100

    Reduce the steps for setting up direnv

--- docs/snippets/steps/contributing-install-direnv.sh
+++ docs/snippets/steps/contributing-install-direnv.sh
@@ -8,11 +8,5 @@ echo "source $HOME/.nix-profile/share/nix-direnv/direnvrc" > ~/.config/direnv/di
 # Hook direnv into your shell
 SHELL_BIN=$(basename $SHELL)
 echo "eval \"\$(direnv hook $SHELL_BIN)\"" >> ~/.${SHELL_BIN}rc
-echo "export DIRENV_WARN_TIMEOUT=0" >> ~/.${SHELL_BIN}rc
-mkdir -p ~/.config/direnv
-cat <<EOF >> ~/.config/direnv/direnv.toml
-[global]
-hide_env_diff = true
-EOF
 eval "$(direnv hook $SHELL_BIN)"
 unset SHELL_BIN
